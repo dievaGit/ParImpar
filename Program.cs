ee1d4d965646e104fd7c3ad4d4b6572ae743e173
Dieva Leon <58702512+dievaGit@users.noreply.github.com>
2020-04-18 23:07:15 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero = 0;
+
+            Console.Write("Escriba un numero positivo:");
+            numero = Convert.ToDouble(Console.ReadLine());
+
+            if(numero > 0)
+             {
+               if(numero % 2 == 0)
+                {
+                    Console.WriteLine("El numero: {0} es par", numero);
+                }
+               else
+                {
+                    Console.WriteLine("El numero: {0} es impar", numero);
+                }
+                          
+             }
+            else
+            {
+                Console.WriteLine("El numero no es valido");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
